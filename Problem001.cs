895db15f0b547d87448ca592b29ad98b5e946c2c
branarci <branarci@gmail.com>
2016-04-19 13:30:09 +1200

    Create Problem001.cs

---
+++ Problem001.cs
@@ -0,0 +1,29 @@
+/**Multiples of 3 and 5
+Problem 1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+
+
+using System;
+class MainClass {
+  public static void Main (string[] args) {
+    int limit = 1000;
+    
+    int result = findMultiples(limit);
+    
+    Console.WriteLine (result);
+  }
+  
+  public static int findMultiples(int roof){
+  	int sum = 0;
+  	for(int i = 0; i < roof; i++){
+  		if((i%3 == 0) || (i%5 == 0)){
+  			sum = sum + i;
+  		}
+  	}
+  	return sum;
+  }
+}
