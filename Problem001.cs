cdee47f24a1ae5b3711b59d6f07786e72476254f
branarci <branarci@gmail.com>
2016-04-19 13:16:11 +1200

    Update Problem001.cs

--- Problem001.cs
+++ Problem001.cs
@@ -1,1 +1,20 @@
-
+using System;
+class MainClass {
+  public static void Main (string[] args) {
+    int limit = 1000;
+    
+    int result = findMultiples(limit);
+    
+    Console.WriteLine (result);
+  }
+  
+  public static int findMultiples(int roof){
+  	int sum = 0;
+  	for(int i = 0; i < roof; i++){
+  		if((i%3 == 0) || (i%5 == 0)){
+  			sum = sum + i;
+  		}
+  	}
+  	return sum;
+  }
+}
