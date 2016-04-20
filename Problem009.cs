d8794395e5656a287425e4ce2a5d529896965ff3
branarci <branarci@gmail.com>
2016-04-20 12:41:23 +1200

    Update Problem009.cs

--- Problem009.cs
+++ Problem009.cs
@@ -10,3 +10,36 @@ Find the product abc.
 */
 
 
+using System;
+class MainClass {
+  public static void Main (string[] args) {
+    
+    pythagoreanTriplet(500, 1000);
+    
+    //Console.WriteLine(answer);
+  }
+  
+  public static void pythagoreanTriplet(int roof, int target){
+  	int actualc = 2;
+	int actualcSquared = 4;
+  	int actualSum = 3;
+  	
+  	for(int a = 1; a<(roof-1); a++){
+  		for(int b = a+1; b<roof; b++){
+  			actualcSquared = a*a + b*b;
+  			
+  			if((Math.Sqrt(actualcSquared) % 1) == 0){
+  				actualc = (int)Math.Sqrt(actualcSquared);
+  				actualSum = a+b+actualc;
+  				
+  				//Console.WriteLine(a +"\t"+ b +"\t"+ actualc +"\t"+ actualSum);
+  				
+  				if(actualSum == target){
+	  				Console.WriteLine(a +"\t"+ b +"\t"+ actualc +"\t"+ actualSum);;
+	  			}
+  			}
+  		}
+  	}
+  }
+  
+}
