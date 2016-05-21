8bbcfe970774deeeb3505250488c949543d8a3e6
R. Tavioni <branarci@gmail.com>
2016-05-22 03:59:58 +1200

    Update Problem015.cs

--- Problem015.cs
+++ Problem015.cs
@@ -8,3 +8,53 @@ How many such routes are there through a 20×20 grid?
 */
 
 
+using System;
+class MainClass {
+  public static void Main (string[] args) {
+    grid(20, 20);
+  }
+
+  public static void grid(int xm, int ym){
+  	int xmax = xm + 1;
+  	int ymax = ym + 1;
+  	
+  	long[,] array = new long[xmax, ymax];
+  	
+  	for(int x = 0; x < xmax; x++){
+	  	for(int y = 0; y < ymax; y++){
+  			array[x,y] = valuefunc(array, x, y);
+  		}
+  	}
+  	
+  	for(int x = 0; x < xmax; x++){
+  		for(int y = 0; y < ymax; y++){
+  			Console.Write(array[x,y] + "\t");
+  		}
+  		Console.WriteLine();
+  	}
+  	
+  	Console.WriteLine("************");
+  	Console.WriteLine(array[xm,ym]);
+  }
+  
+  public static long valuefunc(long[,] a, int x, int y){	
+	long value = 0;
+	
+	if((x == 0) && (y == 0)){
+  		value = 0;
+  	}else if(x == 0){
+  		value = y;
+  	}else if(y == 0){
+  		value = x;
+  	}else if(x == 1){
+  		value = y + 1;
+  	}else if(y == 1){
+  		value = x + 1;
+  	}else{
+  		value = a[x-1,y] + a[x,y-1];
+  	}
+
+  	return value;
+  }
+  
+}
