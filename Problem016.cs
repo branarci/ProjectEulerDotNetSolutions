0a6989017be05080f202e7cd797eaf901c9651a0
R. Tavioni <branarci@gmail.com>
2016-05-22 17:25:32 +1200

    Update Problem016.cs

--- Problem016.cs
+++ Problem016.cs
@@ -1,9 +1,9 @@
 /**Power digit sum
 Problem 16
 Published on Friday, 3rd May 2002, 06:00 pm; Solved by 149492; Difficulty rating: 5%
-215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-What is the sum of the digits of the number 21000?
+What is the sum of the digits of the number 2^1000?
 */
 
 
